7b29ec8bebfa32ac3c4a4040b35cfaa5a17c4c66
Jarrod Doyle <thejarroddoyle@gmail.com>
2024-01-26 16:51:38 +0000

    Add a camera, render a rect, and set the clear colour

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use bevy::{prelude::*, window::PresentMode};
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Harenae".into(),
-                resolution: (1280.0, 720.0).into(),
-                present_mode: PresentMode::AutoVsync,
-                ..default()
-            }),
+    let window_plugin = WindowPlugin {
+        primary_window: Some(Window {
+            title: "Harenae".into(),
+            resolution: (1280., 720.).into(),
+            present_mode: PresentMode::AutoVsync,
             ..default()
-        }))
+        }),
+        ..default()
+    };
+
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(window_plugin)
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(Color::rgb_u8(45, 42, 46)))
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(50.0, 100.0)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+        ..default()
+    });
 }
