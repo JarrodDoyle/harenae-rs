4e47cf5aba6f1e469a754d8cc27e5c00aa253292
Jarrod Doyle <thejarroddoyle@gmail.com>
2024-01-26 16:32:56 +0000

    Change window title, resolution, and enable vsync

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Harenae".into(),
+                resolution: (1280.0, 720.0).into(),
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
