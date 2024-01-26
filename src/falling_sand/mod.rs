9e103c0c0508e735beb6d7c2c9225499e52d4961
Jarrod Doyle <thejarroddoyle@gmail.com>
2024-01-26 17:43:13 +0000

    Set falling sand chunk texure to random colours each frame

--- src/falling_sand/mod.rs
+++ src/falling_sand/mod.rs
@@ -11,12 +11,14 @@ use bevy::{
     sprite::SpriteBundle,
     transform::components::Transform,
 };
+use rand::random;
 
 pub struct FallingSandPlugin;
 
 impl Plugin for FallingSandPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_systems(Startup, setup);
+        app.add_systems(Update, update_chunk_texture_system);
     }
 }
 
@@ -34,9 +36,44 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
 
     let image_handle = images.add(image);
 
-    commands.spawn(SpriteBundle {
-        texture: image_handle,
-        transform: Transform::from_translation(Vec3::new(256., 0., 0.)),
-        ..default()
-    });
+    commands
+        .spawn(Chunk {
+            width: 256,
+            height: 256,
+        })
+        .insert(SpriteBundle {
+            texture: image_handle,
+            transform: Transform::from_translation(Vec3::new(256., 0., 0.)),
+            ..default()
+        });
+}
+
+#[derive(Component)]
+pub struct Chunk {
+    width: usize,
+    height: usize,
+}
+
+pub fn update_chunk_texture_system(
+    mut images: ResMut<Assets<Image>>,
+    mut chunk: Query<(&mut Chunk, &Handle<Image>)>,
+) {
+    // We know for now there's only one chunk
+    let chunk = chunk.get_single_mut();
+    if chunk.is_err() {
+        return;
+    }
+
+    let (chunk, image_handle) = chunk.unwrap();
+    let image = images.get_mut(image_handle).unwrap();
+    for y in 0..chunk.height {
+        for x in 0..chunk.width {
+            // Just set each pixel to random colours for now
+            let index = (x + y * chunk.width) * 4;
+            image.data[index] = random::<u8>();
+            image.data[index + 1] = random::<u8>();
+            image.data[index + 2] = random::<u8>();
+            image.data[index + 3] = 255;
+        }
+    }
 }
